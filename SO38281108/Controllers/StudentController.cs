42a5b73ea6fe379cf56969854cc33e23c2eceae8
crowcoder <crowcoder@gmail.com>
2016-07-09 08:27:07 -0400

    added studenti

--- SO38281108/Controllers/StudentController.cs
+++ SO38281108/Controllers/StudentController.cs
@@ -8,13 +8,20 @@ using System.Web.Http;
 
 namespace SO38281108.Controllers
 {
+    
+
     public class StudentController : ApiController
     {
+        public static List<Student> studenti = new List<Student>()
+        {
+            new Student {ID = 1, Nume = "Radu", Prenume = "John", Nota = 10, Status = true },
+        };
+
         [HttpPost]
         public IHttpActionResult Post([FromBody] Student student)
         {
             if (student == null) return BadRequest();
-            
+            studenti.Add(student);
             return CreatedAtRoute("StudentApi", new { ID = student.ID }, student);
         }
     }
